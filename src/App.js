7d74d5cebc7065cd8c85d36f6a2a039f8743da41
Jeff Soriano <soriano.jeffm@gmail.com>
2023-11-16 21:10:51 -0800

    Add initial fetch for spells

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+// Import everything needed to use the `useQuery` hook
+import { useQuery, gql } from '@apollo/client'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const GET_LOCATIONS = gql`
+  query Classes($level: IntFilter) {
+    classes {
+      name
+      spellcasting {
+        spellcasting_ability {
+          name
+        }
+      }
+      spells(level: $level) {
+        name
+        desc
+      }
+      class_levels {
+        spellcasting {
+          cantrips_known
+          spell_slots_level_1
+          spell_slots_level_2
+          spell_slots_level_3
+          spell_slots_level_4
+          spell_slots_level_5
+          spell_slots_level_6
+          spell_slots_level_7
+          spell_slots_level_8
+          spell_slots_level_9
+          spells_known
+        }
+      }
+    }
+  }
+`
+function DisplaySpells() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error : {error.message}</p>
+
+  return data.classes.map(({ name, spellcasting, spells }) => (
+    <div key={name}>
+      <h3>{name}</h3>
+      <b>Spellcasting:</b>
+      {spellcasting?.spellcasting_ability.name}
+      <br />
+      <b>Spells</b>
+      {spells.map(({ name, desc }, index) => (
+        <div key={index}>
+          <b>{name}</b>
+          <p>{desc}</p>
+        </div>
+      ))}
+      <br />
     </div>
-  );
+  ))
 }
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <br />
+      <DisplaySpells />
+    </div>
+  )
+}
